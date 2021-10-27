578cc908be46c3ed022ca62f2dc558d351b7706d
ybdesire <ybdesire@gmail.com>
2021-10-27 17:41:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_breast_cancer, load_iris
 from sklearn.model_selection import train_test_split
-import pandas as pd
 
 data = load_iris(as_frame=True)# load as dataframe
 x_data = data.data.to_numpy()
